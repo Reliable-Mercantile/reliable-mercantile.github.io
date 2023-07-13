6761abda13747787c7730a73acb8c77ce3acb9e8
Madeleine Goebel <madeleine.goebel@gmail.com>
2023-07-13 10:49:59 -0400

    Update robot and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './pages/Layout';
-import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Services } from './pages/Services';
 import { ContactUs } from './pages/ContactUs';
