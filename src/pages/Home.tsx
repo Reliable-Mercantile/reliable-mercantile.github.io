3ecc1c83baccab43562bd05225876818aec1ffd0
Madeleine Goebel <madeleine.goebel@gmail.com>
2023-08-02 00:53:57 -0400

    make landing page still limited to max width

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 export const Home = () => {
     return (
-        <>
+        <div style={{maxWidth: "1240px", width: "90vw"}}>
         <Typography variant='h3'>Welcome to the Reliable Mercantile!</Typography>
         <Typography>The Reliable Mercantile is a small collective of textile and new media artists, software developers, academics, teachers, and at least one TikToker. We’ve organized to support each other in our professional endeavors and help make each other’s weird lives and professions possible. There’s no job too grand, nor task too trivial: you can trust Reliable Mercantile to execute with excellence and whimsy the tasks and responsibilities levied upon us on chance encounters and special occasions.
         </Typography>
-        </>
+        </div>
     )
 };
